4f7b78972c6027e4e1d0e2c86ee81e519527ad00
aytung94 <aytung94@gmail.com>
2016-02-21 22:20:50 -0600

    Itemssssss

--- Assignment3/src/Assignment3/Clothing.java
+++ Assignment3/src/Assignment3/Clothing.java
@@ -1,26 +1,11 @@
 package Assignment3;
 
 public class Clothing extends Item 
-{
-
-	
+{   
 	// variables, constructors as necessary
-	public Clothing(String name, double price, int quantity, double weight)
+	public Clothing(String name, double price, int quantity, int weight)
 	{
 		super(name, price, quantity, weight);
 	}
 	
-	float calculatePrice () 
-	{
-		float final_price = 0;
-		// Insert price calculation here
-		return final_price;
-	}
-	
-	void printItemAttributes () 
-	{
-		//Print all applicable attributes of this sub-class
-	}
-	
-
 }
--- Assignment3/src/Assignment3/Item.java
+++ Assignment3/src/Assignment3/Item.java
@@ -6,9 +6,9 @@ public class Item
 	protected String name;
 	protected double price;
 	protected int quantity;
-	protected double weight;
+	protected int weight;
 	
-	public Item(String name, double price, int quantity, double weight)
+    public Item(String name, double price, int quantity, int weight)
 	{
 		this.name = name;
 		this.price = price;
@@ -16,17 +16,64 @@ public class Item
 		this.weight = weight;
 	}
 	
-	float calculatePrice () 
+	double calculatePrice () 
 	{
-		float final_price = 0;
+		// Sales Tax
+        double tax_cost = this.price * (.1); // 10% tax
+                
+        // Shipping cost 
+        double shipping_cost = 20*(this.weight); 
+        
+        // Final price for the total quantity of item 
+        double final_price = quantity * (tax_cost + shipping_cost + price);
+        
 		// Insert price calculation here
 		return final_price;
 	}
 	
 
-	void printItemAttributes () 
-	{
+	void printItemAttributes() 
+	{   
+        // String for attributes
+        String output_name = "Item: " + this.name + "\n";
+        String output_price = "Price: $" + String.format("%1$,.2f", this.price) + "\n";
+        String output_quantity = "Quantity: " + this.quantity + "\n";;
+        String output_weight = "Weight: " + (this.weight*this.quantity) + "\n";
+        
 		//Print all applicable attributes of this class
+        System.out.println(output_name + output_price + output_quantity + output_weight);
 	}
+    
+    public String getName(){
+        return name;
+    }
+    
+    public double getPrice(){
+        return price;
+    }    
+ 
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setPrice(double price){
+        this.price = price;
+    }    
+ 
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
 
 }
